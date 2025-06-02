b0a37a385651b2bad3fe9adcb30c7430d0cbe8dc
Segundavid-Dev <segdavid03@gmail.com>
2025-06-02 10:11:41 +0100

    testing with hikaflow

--- index.mjs
+++ index.mjs
@@ -1,15 +1,28 @@
-// Reading from a file
+// // Reading from a file
 
 import fs from "fs";
-// Es module file system import
+// // Es module file system import
 
-const myValue = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(myValue);
+// const myValue = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(myValue);
 
-// writing to a file
-const text =
-  "Hello, my name is Segun David and i am frontend engineer with increasing knowledge in the backend field";
+// // writing to a file
+// const text =
+//   "Hello, my name is Segun David and i am frontend engineer with increasing knowledge in the backend field";
 
-// This dosen't return anything any value except for the new file
-fs.writeFileSync("./txt/created.txt", text);
-console.log("writing to file is successful");
+// // This dosen't return anything any value except for the new file
+// fs.writeFileSync("./txt/created.txt", text);
+// console.log("writing to file is successful");
+
+// using the readFile Asynchronously to read different files in the background
+fs.readFile("./txt/created.txt", "utf-8", (err, data) => {
+  if (err) return console.log("Error ❌");
+  console.log(data);
+});
+
+const values = "lorem lorem lorem lorem lorem lorem lorem ipsum ipsum dolor";
+
+fs.writeFile("./txt/start.txt", `${values}`, "utf-8", (err) => {
+  console.log("your file has been written!");
+});
+console.log("reading file....");
