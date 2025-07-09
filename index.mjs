7d6a3dff5152fb5df9d15770a9af22ee738b409c
Segundavid-Dev <segdavid03@gmail.com>
2025-07-09 12:59:45 +0100

    templating HTML

--- index.mjs
+++ index.mjs
@@ -4,6 +4,8 @@
 import { readFile, readFileSync } from "node:fs";
 import { writeFile } from "node:fs";
 import { createServer } from "node:http";
+import { url } from "node:inspector";
+import { url } from "node:inspector/promises";
 
 readFile("./txt/test.txt", "utf-8", (err, data) => {
   console.log(data);
@@ -20,17 +22,56 @@ writeFile("./txt/test.md", writeText, "utf-8", (err) => {
   }
 });
 
-const server = createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-  if (req.url === "/name") {
-    res.end(JSON.stringify({ name: "Segun David Olasunkanmi" }));
-  } else {
-    res.end(json);
-  }
-});
+// const server = createServer((req, res) => {
+//   res.writeHead(200, {
+//     "Content-Type": "application/json",
+//   });
+//   if (req.url === "/name") {
+//     res.end(JSON.stringify({ name: "Segun David Olasunkanmi" }));
+//   } else {
+//     res.end(json);
+//   }
+// });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to server actions");
+// server.listen(8000, "127.0.0.1", () => {
+//   console.log("listening to server actions");
+// });
+
+// SERVER
+const tempOverview = readFileSync(
+  `${__dirname}/dev-data/template-overview.html`,
+  "utf-8"
+);
+const tempCard = readFileSync(
+  `${__dirname}/dev-data/template-card.html`,
+  "utf-8"
+);
+const tempProduct = readFileSync(
+  `${__dirname}/dev-data/template-product.html`,
+  "utf-8"
+);
+
+const data = readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = createServer((req, res) => {
+  const pathName = req.url;
+
+  // overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    res.end(tempOverview);
+  }
+
+  // product page
+  else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  }
 });
